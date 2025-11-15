d556bf3ab80b739be984a4f2793c2018c259bcfe
Lucas0ffcs <lacerdalucas511@gmail.com>
2025-11-14 23:49:42 -0300

    Estrutura classe Program e seu método main

--- exerc_composicao2/src/application/Program.java
+++ exerc_composicao2/src/application/Program.java
@@ -1,4 +1,46 @@
 package application;
 
+import entities.Client;
+import entities.Item;
+
+import java.util.Scanner;
+
 public class Program {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter client data: ");
+
+        System.out.println("Name: ");
+        String clientName = sc.next();
+
+        System.out.println("Email: ");
+        String clientEmail = sc.next();
+
+
+        Client client1 = new Client(clientName, clientEmail);
+
+        System.out.print("How many items in the order? ");
+        int z = sc.nextInt();
+
+        for (int i = 1; i<=z; i++){
+            System.out.println("Enter item #" + i + " data:");
+            System.out.println("Item name: ");
+            String itemName = sc.next();
+
+            System.out.println("Price: ");
+            Double itemPrice = sc.nextDouble();
+            Item item = new Item(itemName, itemPrice);
+
+        }
+
+
+
+
+
+
+
+
+
+    }
 }
